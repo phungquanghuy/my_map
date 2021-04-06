c8dfb2bfb63c519268d8458c533a3e09ba1441f3
phungquanghuy <qhuy1501@gmail.com>
2021-04-06 22:40:51 +0700

    Update layout for android

--- lib/screens/map_main.dart
+++ lib/screens/map_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mymap/ultis/constant.dart';
 
 class MapMain extends StatefulWidget {
   MapMain({Key key}) : super(key: key);
@@ -10,18 +11,13 @@ class MapMain extends StatefulWidget {
 
 class _MapMainState extends State<MapMain> {
   GoogleMapController controller;
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        mapType: MapType.normal,
+        initialCameraPosition: MapConstant.kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           controller = controller;
         },
---
+++ lib/ultis/constant.dart
@@ -0,0 +1,8 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapConstant {
+  static final CameraPosition kGooglePlex = CameraPosition(
+    target: LatLng(21.0170591, 105.8273881),
+    zoom: 11.8,
+  );
+}
